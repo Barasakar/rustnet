b9cb9e9c3c0017ce58a4efa7e9cde1f4daf0d80e
barasakar <jlinpoughkeepsieday@gmail.com>
2025-11-04 09:13:25 -0600

    reworked dense layer data structure to Array2&1

--- src/dense.rs
+++ src/dense.rs
@@ -1,12 +1,12 @@
 
 use rand::Rng;
-
+use ndarray::{Array1, Array2};
 
 pub struct Dense {
     pub input_size: usize,
     pub output_size: usize,
-    pub weights: Vec<Vec<f32>>,
-    pub biases: Vec<f32>,
+    pub weights: Array2<f32>,
+    pub biases: Array1<f32>,
 }
 
 impl Dense {
@@ -15,16 +15,14 @@ impl Dense {
         let limit = (6.0 / (input_size + output_size) as f32).sqrt();
         // Initialize weights with random values between -limit and limit; Xavier initialization
         let mut rng = rand::thread_rng();
-        let mut weights : Vec<Vec<f32>> = Vec::new();
-        for _ in 0..output_size {
-            let mut row : Vec<f32> = Vec::new();
-            for _ in 0..input_size {
+        let mut weights : Array2<f32> = Array2::zeros((input_size, output_size));
+        for i in 0..input_size {
+            for j in 0..output_size {
                 let random_value = rng.gen_range(-limit..limit);
-                row.push(random_value);
+                weights[[i, j]] = random_value;
             }
-            weights.push(row);
         }
-        let biases : Vec<f32> = vec![0.0; output_size];
+        let biases : Array1<f32> = Array1::zeros(output_size);
 
         // Return 
         Dense {
@@ -33,5 +31,9 @@ impl Dense {
             weights,
             biases,
         }
+    }
+    pub fn forward(&mut self, input: &Array2<f32>) -> Array2<f32> {
+        let mut output = input.dot(&self.weights) + &self.biases;
+        output
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod dense;
+use ndarray::Array2;
 
 fn main() {
-    let dense_layer = dense::Dense::new(4, 3);  
+    let mut dense_layer = dense::Dense::new(4, 3);  
     println!("Dense layer created with input size {} and output size {}", dense_layer.input_size, dense_layer.output_size);
     println!("Weights: {:?}", dense_layer.weights);
     println!("Biases: {:?}", dense_layer.biases);
+    // Test forward
+
+    let first_layer : Array2<f32> = dense_layer.forward(&Array2::zeros((1, 4)));
+    println!("Output of forward pass: {:?}", first_layer);
 }

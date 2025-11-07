f2aa14df7b1dde787015703998c3ac0d453155ff
barasakar <jlinpoughkeepsieday@gmail.com>
2025-11-07 15:08:18 -0600

    finished implementing backward pass for Dense Layer

--- src/dense.rs
+++ src/dense.rs
@@ -1,12 +1,15 @@
 
 use rand::Rng;
-use ndarray::{Array1, Array2};
+use ndarray::{Array1, Array2, Axis};
 
 pub struct Dense {
     pub input_size: usize,
     pub output_size: usize,
     pub weights: Array2<f32>,
     pub biases: Array1<f32>,
+    pub input_cache: Option<Array2<f32>>, 
+    pub grad_weights_cache: Option<Array2<f32>>,
+    pub grad_biases_cache: Option<Array1<f32>>,
 }
 
 impl Dense {
@@ -30,10 +33,23 @@ impl Dense {
             output_size,
             weights,
             biases,
+            input_cache: None,
+            grad_weights_cache: None,
+            grad_biases_cache: None,
         }
     }
     pub fn forward(&mut self, input: &Array2<f32>) -> Array2<f32> {
         let mut output = input.dot(&self.weights) + &self.biases;
+        self.input_cache = Some(input.clone());
         output
+    }
+    
+    pub fn backward(&mut self, grad_output: &Array2<f32>) -> Array2<f32> {
+        let grad_input = grad_output.dot(&self.weights.t());
+        let grad_weights = self.input_cache.as_ref().unwrap().t().dot(grad_output);
+        let grad_biases = grad_output.sum_axis(Axis(0));
+        self.grad_weights_cache = Some(grad_weights); // Bug here. How to fix?
+        self.grad_biases_cache = Some(grad_biases);
+        grad_input
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ mod dense;
 use ndarray::Array2;
 
 fn main() {
+    // TEST DENSE LAYER
     let mut dense_layer = dense::Dense::new(4, 3);  
     println!("Dense layer created with input size {} and output size {}", dense_layer.input_size, dense_layer.output_size);
     println!("Weights: {:?}", dense_layer.weights);
     println!("Biases: {:?}", dense_layer.biases);
-    // Test forward
-
+    
+    // TEST FORWARD PASS
     let first_layer : Array2<f32> = dense_layer.forward(&Array2::zeros((1, 4)));
     println!("Output of forward pass: {:?}", first_layer);
+
+    // TEST BACKWARD PASS
+    dense_layer.backward(&first_layer);
+    println!("Gradient w.r.t weights: {:?}", dense_layer.grad_weights_cache);
+    println!("Gradient w.r.t biases: {:?}", dense_layer.grad_biases_cache);
 }
